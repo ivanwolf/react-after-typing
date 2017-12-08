23b481bba9ebfc0f4b5567cea32cd490a0d46e56
Ivan Wolf <ivanwolf15@gmail.com>
2017-12-08 20:06:49 -0300

    first commit

--- demo/src/index.js
+++ demo/src/index.js
@@ -1,13 +1,31 @@
-import React, {Component} from 'react'
-import {render} from 'react-dom'
+import React, {Component} from 'react';
+import {render} from 'react-dom';
 
-import Example from '../../src'
+import afterTyping from '../../src';
+
+const Input = props => (
+  <input type="text" {...props} />
+);
+
+const InputWithCallback = afterTyping(Input);
+
+const toggleShow = state => ({
+  show: !state.show,
+});
 
 class Demo extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      show: true,
+    }
+  }
   render() {
     return <div>
       <h1>react-after-typing Demo</h1>
-      <Example/>
+      <InputWithCallback afterTyping={() => this.setState(toggleShow)}/>
+      <br/> 
+      {this.state.show && <span>This text is hidden after typing</span>}
     </div>
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,9 +1,48 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
-export default class extends Component {
-  render() {
-    return <div>
-      <h2>Welcome to React components</h2>
-    </div>
+
+export default function(Target) {
+  class AfterTypingInput extends Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        timeout: null,
+      };
+      this.handleOnChange = this.handleOnChange.bind(this);
+    }
+
+    handleOnChange(ev) {
+      if (typeof this.props.onChange === 'function') {
+        this.props.onChange(ev);
+      }
+      console.log('hola')
+      this.setState((state, props) => {
+        clearTimeout(state.timeout);
+        return {
+          timeout: setTimeout(props.afterTyping, props.time),
+        };
+      });
+    }
+
+    render() {
+      const {
+        onChange, afterTyping, time, ...rest
+      } = this.props;
+      return (
+        <Target onChange={this.handleOnChange} {...rest} />
+      );
+    }
   }
-}
+
+  AfterTypingInput.defaultProps = {
+    time: 500,
+  };
+
+  AfterTypingInput.propTypes = {
+    afterTyping: PropTypes.func.isRequired,
+    time: PropTypes.number,
+  };
+
+  return AfterTypingInput;
+};
